6a1ff7257aa348ce7c012bfa83913a7264cae9e3
Leonardo Muñoz <leofmunoz94@gmail.com>
2026-07-22 10:01:38 -0500

    ejer 3

--- eje3.js
+++ eje3.js
@@ -1,13 +1,1 @@
-for (let x=2;x<=1000;x++){
-    let esprimo = true;
-    for (let y=2;x>y;y++){
-        if(x%y===0){
-            esprimo = false;
-            break;
-        }
-       
-    }
-    if(esprimo===true){
-            console.log(x)
-    }
-}
+for for 
